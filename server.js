d451013b1da9c74e64ed250d09103b9be1bd6630
Thomas Casasnovas <thomas.casasnovas@gmail.com>
2022-11-08 13:00:20 +0100

    link mongoDB to api

--- server.js
+++ server.js
@@ -9,6 +9,24 @@ const app = express();
 
 app.use(cors(corsOptions));
 
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+
+const url= "mongodb+srv://Mastoxic:vyajd2Zg2HjXDXA@factures.5rjze8y.mongodb.net/?retryWrites=true&w=majority";
+// linking the api to the mongoDB database
+mongoose.connect(url,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+).then(() => {console.log('Database Connected');
+            console.log("Successfully coneected to MongoDB");
+    }).catch(err =>{
+        console.log(err);
+        console.log('Could not connect to MongoDB');
+});
+
 // parse requests of content-type - application/json
 app.use(express.json());
 
